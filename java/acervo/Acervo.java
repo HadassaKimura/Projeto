448c0ca93d09922175a0efc1f71714335bfb7272
HadassaKimura <69400710+HadassaKimura@users.noreply.github.com>
2021-11-29 11:29:34 -0300

    Update Acervo.java

--- java/acervo/Acervo.java
+++ java/acervo/Acervo.java
@@ -11,31 +11,45 @@ public class Acervo {
     public static ArrayList<LivroBiblioteca> getListaLivros() {
         return listaLivros;
     }
+
     //Adicionar um objeto na lista
-    public static void adicionar(LivroBiblioteca l){
+    public static void adicionar(LivroBiblioteca l) {
         listaLivros.add(l);
     }
 
     //Lista todos os objetos da lista
-    public static String listar(){
+    public static String listar() {
         String saida = "";
         int i = 1;
-        for(LivroBiblioteca l :listaLivros){
-            saida +="\n====== Livro Nº " + (i++) + "=====\n";
-            saida +=l.imprimir() + "\n";
+        for (LivroBiblioteca l : listaLivros) {
+            saida += "\n====== Livro Nº " + (i++) + "=====\n";
+            saida += l.imprimir() + "\n";
         }
         return saida;
     }
 
     //Pesquisar por genero
-    public static int pesquisar(String genero){
-        int qtd =0;
+    public static int pesquisarGenero(String genero) {
+        int qtd = 0;
 
-        for(LivroBiblioteca l : listaLivros){
-            if(l.getGenero().equalsIgnoreCase(genero)){
+        for (LivroBiblioteca l : listaLivros) {
+            if (l.getGenero().equalsIgnoreCase(genero)) {
                 qtd++;
             }
         }
-       return qtd;
+        return qtd;
+    }
+
+    //Pesquisar por titulo
+    public static int pesquisarTitulo(String titulo) {
+        int qtd1 = 0;
+        for (LivroBiblioteca l : listaLivros) {
+            if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                qtd1++;
+            }
+        }
+        return qtd1;
     }
 }
+
+
